0521974a1f328152fa7d5086b5ab2444692fb0d9
Chris Harris <chrisharriscjh@gmail.com>
2021-05-22 10:40:32 +0900

    capture function macro working

--- src/Jot.jl
+++ src/Jot.jl
@@ -3,6 +3,11 @@ module Jot
 # IMPORTS
 using JSON, Parameters
 
+# EXPORTS
+export AWSConfig, ImageConfig, LambdaFunctionConfig, Config
+export ImageDefinition, Image
+export set_lambda_function
+
 # EXCEPTIONS
 struct InterpolationNotFoundException <: Exception 
   interpolation::String
@@ -16,7 +21,7 @@ end
 @with_kw struct ImageConfig
   name::String
   tag::String = "latest"
-  dependencies::Array{String} = Array{String}()
+  dependencies::Vector{String} = []
   base::String = "1.6.0"
   runtime_path::String = "/var/runtime"
   julia_depot_path::String = "/var/julia"
@@ -25,42 +30,67 @@ end
 
 @with_kw struct LambdaFunctionConfig
   name::String
-  role::String
+  role::String = "LambdaExecutionRole"
   timeout::Int = 30
   memory_size::Int = 1000
 end
 
-@Base.kwdef struct Config
+@with_kw struct Config
   aws::AWSConfig
   image::ImageConfig
   lambda_function::LambdaFunctionConfig
 end
 
-# GLOBALS
-@Base.kwdef struct Builtins
-  scripts_path::String
-  image_path::String
-  function_path::String
-  template_path::String
-  template_scripts_path::String
-  source_files_path::String
-  special_folder_names::Array{String}
-  default_config_path::String
-  script_templates_path::String
-  required_packages::Array{String}
+function Config(
+  aws_account_id::String,
+  aws_region::String,
+  function_name::String,
+)::Config
+  Config(
+    AWSConfig(account_id = aws_account_id, region = aws_region),
+    ImageConfig(name = function_name),
+    LambdaFunctionConfig(name = function_name), 
+  )
 end
 
-const builtins = Builtins(
-  scripts_path = "./scripts",
-  image_path = "./image",
-  function_path = "./function",
-  template_path = "./template",
-  template_scripts_path = "./template/scripts",
-  source_files_path = "./template/image",
-  special_folder_names = ["_runtime", "_depot"],
-  default_config_path = "./config.json",
-  script_templates_path = "./template/scripts",
-  required_packages = ["HTTP", "JSON"],
-)
+mutable struct ImageDefinition
+  func_expr::Union{Nothing, Expr}
+  config::Union{Nothing, Config}
+end
+
+function ImageDefinition()::ImageDefinition
+  ImageDefinition(nothing, nothing)
+end
+
+function ImageDefinition(config::Config)::ImageDefinition
+  ImageDefinition(nothing,  config)
+end
+
+macro set_lambda_function(image_definition, f_expr)
+  Expr(:(=), :($(esc(image_definition)).func_expr), Expr(:quote, f_expr))
+end
+
+macro set_lambda_function1(image_definition, f_expr)
+  return quote
+    local id = eval($(esc(image_definition)))
+    id.func_expr = :(eval($$(esc(f_expr))))
+  end
+end
+
+macro set_lambda_function2(image_definition, f_expr)
+  return quote
+    local id = eval($(esc(image_definition)))
+    id.func_expr = esc($(esc(f_expr)))
+  end
+end
+
+Base.@kwdef struct InvocationResponse
+  response::String
+end
+
+Base.@kwdef struct InvocationError
+  errorType::String
+  errorMessage::String
+end
 
 end
