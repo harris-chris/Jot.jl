ddfb40f253c457b31739bf53d9b5ec4170eabe98
Chris Harris <chrisharriscjh@gmail.com>
2021-06-03 13:13:22 +0900

    bootstrap WIP

--- src/Jot.jl
+++ src/Jot.jl
@@ -210,8 +210,9 @@ end
 function start_image_locally(image::Image, detached::Bool)
   args = ["-p", "9000:8080", "$(get_image_uri_string(image.definition.config))"]
   detached && push!(args, "-d")
+  out = read(stdout, String)
   run(`docker run $args`, wait=false)  
-  read(stdout)
+  @show out
   # read(`docker run $args`, String)
 end
 
