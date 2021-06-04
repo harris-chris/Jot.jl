6a200a7229faa8852d7edffba241da456eb7f14b
Chris Harris <chrisharriscjh@gmail.com>
2021-06-04 20:00:38 +0900

    Passing JotTest1 test

--- test/JotTest1/src/JotTest1.jl
+++ test/JotTest1/src/JotTest1.jl
@@ -2,8 +2,9 @@ module JotTest1
 
 function response_func(s::String)::String
   @debug pwd()
-  this_path = Base.moduleroot(JotTest1) |> pathof
-  open(joinpath(this_path, "response_suffix"), "r") do rs
+  package_root = Base.moduleroot(JotTest1) |> pathof |> splitpath
+  rs_path = joinpath(package_root[begin:end-2]..., "response_suffix")
+  open(rs_path, "r") do rs
     response_suffix = readchomp(rs)
     s * response_suffix
   end
