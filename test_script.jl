4de9c8f99eab2679f2a751fc28613f7d19210fd7
harris-chris <chrisharriscjh@gmail.com>
2022-12-05 21:11:56 +0900

    Add get_invocation_precompile_time

--- test_script.jl
+++ test_script.jl
@@ -5,20 +5,17 @@ using Jot
 
 println("$JOT_OBSERVATION")
 
-f = get_lambda_function("jottest1")
+f = get_lambda_function(ARGS[begin])
 (res, log) = invoke_function_with_log(Dict("double"=>2), f)
 @info "function returned result $res"
 @info "request_id"
 @show log.RequestId
-@info "\nShowing debug events"
-foreach(log.cloudwatch_log_debug_events) do event
+@info "\nShowing events"
+foreach(log.cloudwatch_log_events) do event
   println(event)
 end
 
-@info "\nShowing user events"
-foreach(log.cloudwatch_log_user_events) do event
-  println(event)
-end
 println("Function run time was $(get_invocation_run_time(log))")
+println("Precompile time was $(get_total_precompile_time(log))")
 show_observations(log)
 
